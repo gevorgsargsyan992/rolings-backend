d22e161f880daaa27a13cfb9e29fdda631ec6ab5
gevorg.sargsyan <gevorg.sargsyan@softconstruct.com>
2024-06-18 15:58:09 +0400

    Added apis for edit tablet status and unassign video

--- src/modules/tablet/tablet.module.ts
+++ src/modules/tablet/tablet.module.ts
@@ -5,9 +5,10 @@ import { TabletService } from "./tablet.service";
 import { Tablet } from "./entities/tablet.entity";
 import { TabletController } from "./tablet.controller";
 import { TabletStatus } from "./entities/tablet-status.entity";
+import { TabletVideo } from "../video/entities/tablet-video.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tablet, TabletStatus])],
+  imports: [TypeOrmModule.forFeature([Tablet, TabletStatus, TabletVideo])],
   providers: [TabletService],
   controllers: [TabletController],
   exports: [TabletService],
